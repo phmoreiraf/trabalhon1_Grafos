9bbf67772cc1d59ada62f1b1da5bf39f52978a2e
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-11-30 23:44:33 -0300

    att

--- codigo/outroMainTeste2.java
+++ codigo/outroMainTeste2.java
@@ -1,90 +1,78 @@
 package codigo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class outroMainTeste2 {
-    public static void main(String[] args) {
+ 
+
+      public static void main(String[] args) {
         // Cria um scanner para ler a entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Pede ao usuário para escolher o tamanho do grafo
-        System.out.println("Escolha o tamanho do grafo:");
-        System.out.println("1. 100 vértices");
-        System.out.println("2. 1000 vértices");
-        System.out.println("3. 10000 vértices");
-        System.out.println("4. 100000 vértices");
-        int choice = scanner.nextInt();
+        // Pede ao usuário para inserir o nome do arquivo
+        System.out.print("Digite o nome do arquivo: ");
+        String filename = scanner.nextLine();
 
-        // Cria o grafo do tamanho escolhido
-        int v;
-        switch (choice) {
-            case 1:
-                v = 100;
-                break;
-            case 2:
-                v = 1000;
-                break;
-            case 3:
-                v = 10000;
-                break;
-            case 4:
-                v = 100000;
-                break;
-            default:
-                System.out.println("Escolha inválida.");
-                return;
-        }
-        Graph g = new Graph(v);
-        for (int i = 0; i < v; i++) {
-            for (int j = i + 1; j < v; j++) {
-                g.addEdge(i, j);
-            }
-        }
+        // Lê o grafo do arquivo
+        Graph g = readGraphFromFile(filename);
 
-        // Pede ao usuário para escolher o método para identificar pontes ou encontrar
-        // um caminho euleriano
-        System.out.println("Escolha o método:");
+        // Pede ao usuário para escolher o método para identificar pontes
+        System.out.println("Escolha o método para identificar pontes:");
         System.out.println("1. Método Naive");
         System.out.println("2. Método de Tarjan");
-        System.out.println("3. Método de Fleury");
-        choice = scanner.nextInt();
+        int choice = scanner.nextInt();
 
-        // Executa o método escolhido e mede o tempo
-        long startTime, endTime;
+        // Identifica as pontes usando o método escolhido
         switch (choice) {
             case 1:
-                startTime = System.nanoTime();
                 g.bridgeNaive();
-                endTime = System.nanoTime();
-                System.out.println(
-                        "Tempo para o método Naive com " + v + " vértices: " + (endTime - startTime) + " nanosegundos");
                 break;
             case 2:
-                startTime = System.nanoTime();
                 g.bridgeTarjan();
-                endTime = System.nanoTime();
-                System.out.println("Tempo para o método de Tarjan com " + v + " vértices: " + (endTime - startTime)
-                        + " nanosegundos");
                 break;
             case 3:
-                startTime = System.nanoTime();
-                int res = g.isEulerian();
-                if (res == 0)
-                    System.out.println("O grafo não é euleriano.");
-                else if (res == 1)
-                    System.out.println("O grafo é semi-euleriano.");
-                else
-                    System.out.println("O grafo é euleriano.");
-
-                // Encontra um caminho euleriano usando o método de Fleury
-                g.fleury(0);
-                endTime = System.nanoTime();
-                System.out.println("Tempo para o método de Fleury com " + v + " vértices: " + (endTime - startTime)
-                        + " nanosegundos");
-                break;
+            System.out.println("Saindo...");
+            scanner.close();
+            break;
             default:
                 System.out.println("Escolha inválida.");
-                return;
+                //return;
+        }
+        
+        // Verifica se o grafo é euleriano, semi-euleriano ou não euleriano
+        int res = g.isEulerian();
+        if (res == 0)
+            System.out.println("O grafo não é euleriano.");
+        else if (res == 1)
+            System.out.println("O grafo é semi-euleriano.");
+        else
+            System.out.println("O grafo é euleriano.");
+
+        // Encontra um caminho euleriano usando o método de Fleury
+        g.fleury(0);
+    }
+
+    // Função para ler um grafo de um arquivo
+    static Graph readGraphFromFile(String filename) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            int V = Integer.parseInt(reader.readLine());
+            Graph g = new Graph(V);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                int v = Integer.parseInt(parts[0]);
+                int w = Integer.parseInt(parts[1]);
+                g.addEdge(v, w);
+            }
+            reader.close();
+            return g;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
